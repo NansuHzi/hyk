ea04bac063c93f2bc370028129d4eda96d199ca1
NansuHzi <1934661441@qq.com>
2022-07-27 10:05:43 +0800

    '创建学生类’

---
+++ user-manager/src/Student.java
@@ -0,0 +1,43 @@
+public class Student {
+    String goToNightClass,goToLibrary,haveClass,doGymnastics,doMath,goJogging,doStudy;
+
+    //上晚自习
+    //泡图书馆
+    //上课
+    //做广播体操
+    //f(x)=ax+b
+    //跑步
+    //学习
+
+    public void goToNightClass(){
+        System.out.println("我们在认真上晚自习");
+
+    }
+
+    public void goToLibrary(){
+        System.out.println("我们泡图书馆");
+    }
+
+    public void haveClass(){
+        System.out.println("我们上课");
+    }
+
+    public void doGymnastics(){
+        System.out.println("我们做广播体操");
+    }
+
+    public void doMath(){
+        System.out.println("我们写题f(x)=ax+b");
+    }
+    public void goJogging(){
+        System.out.println("我们跑步");
+
+    }
+    public void doStudy(){
+        System.out.println("我们学习");
+    }
+
+
+
+}
+
